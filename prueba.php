5f9b1929d2a4f9a2ed41c855fc0ca14e4836a657
mlls2014 <manlucsan@gmail.com>
2021-10-14 12:21:30 +0200

    sesion10

--- prueba.php
+++ prueba.php
@@ -1,10 +1,6 @@
 <?php
-for($i=0;$i< 10; $i++){
-   if($i == 5) continue;
-   // En la iteración 5 se salta a la siguiente iteración
-   if($i == 8)break;
-   // En la iteración 8 queremos que termine el bucle
-   echo "Iteración número: $i <br>"; // Código a ejecutar
-}
-echo"El bucle ha terminado.";
+setcookie("color","rojo");
+echo $_COOKIE["color"]; /*color es ‘rojo’*//* Más código y funciones... */
+setcookie("color","azul");
+echo $_COOKIE["color"];
    
---
+++ tema4/cookies/jsoncookies.php
@@ -0,0 +1,25 @@
+<?php
+/*Guardar elemento complejo como cookie
+Lo ideal es convertir en cadena de texto el elemento complejo con la función json_encode
+Y para recuperar el elemento utilizar json_decode
+Estas funciones se utilizan en muchos otros tipos de problemas
+*/
+if (!isset($_COOKIE['cards'])) {
+   //Establecemos la cookie
+   $cardArray = array(
+      'CARD 1' => array('FRONT I', 'BACK I'),
+      'CARD 2' => array('FRONT 2', 'BACK 2')
+   );
+
+   //Convierto el array en una cadena json
+   $json = json_encode($cardArray);
+   setcookie('cards', $json);
+} else {
+   $cookie = $_COOKIE['cards'];
+   // $cookie = stripslashes($cookie); // setcookie incluye \ antes de las comillas y tenemos que quitarlos para que no falle json_decode
+   $savedCardArray = json_decode($cookie, true);
+   //Para mostrar la matriz correctamente
+   echo '<pre>';
+   print_r($savedCardArray);
+   echo '</pre>';
+}
