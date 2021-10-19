42db3899c13c783c7de9ad6759be4654f90b160d
mlls2014 <manlucsan@gmail.com>
2021-10-19 21:04:40 +0200

    sesion11b

--- tema4-cookies-sesiones/Tema4-CookiesSesiones2/Login/frmInicio.php
+++ tema4-cookies-sesiones/Tema4-CookiesSesiones2/Login/frmInicio.php
@@ -3,11 +3,10 @@
 Página de inicio a nuestro portal, una vez hemos logueado correctamente. Esto lo comprobamos 
 con isset($_SESSION['logueado'])
 */
-  session_start();
+?> session_start();
   if(!isset($_SESSION['logueado'])){
      header ("Location: frmLogin.php?error=fuera");
   }
-?>
 
 <html>
   <head lang="es">
