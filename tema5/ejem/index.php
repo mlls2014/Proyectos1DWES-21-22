2251b345854ff2485a0d6afe16d4816c4734557f
mlls2014 <manlucsan@gmail.com>
2021-10-21 14:08:51 +0200

    sesion12

---
+++ tema5/ejem/index.php
@@ -0,0 +1,40 @@
+<?php
+interface Imprimible
+{
+   public function imprime();
+}
+
+class Curriculum implements Imprimible
+{
+   public function imprime(){
+      return "imprime el currículum";
+   }
+}
+
+class Informe implements Imprimible
+{
+   public function imprime(){
+      return "imprime el informe";
+   }
+}
+
+class Impresora
+{
+   public function imprimir(Imprimible $algo){
+      echo $algo->imprime();
+   }
+}
+
+abstract class Main
+{
+   public static function run(){
+      $impresora = new Impresora();
+
+      $impresora->imprimir(new Curriculum());
+      echo "<br>";
+      $impresora->imprimir(new Informe());
+      $impresora->imprimir("asjo");
+   }
+}
+
+Main::run();
