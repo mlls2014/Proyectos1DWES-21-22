9bb3c9794e80cfe12964d029838c8dda13ebaba9
mlls2014 <manlucsan@gmail.com>
2021-10-27 16:45:30 +0200

    ses16

--- tema6/bd.sql
+++ tema6/bd.sql
@@ -13,3 +13,9 @@ CREATE TABLE IF NOT EXISTS `clientes` (
   KEY `nombre` (`nombre`),
   FULLTEXT KEY `apellidos` (`apellidos`)
 ) ENGINE=InnoDB COMMENT='tabla de clientes';
+
+INSERT INTO `clientes` (`id`, `nombre`, `apellidos`, `telefono`, 
+`codigo_postal`, `edad`, `sexo`, `ciudad`, `contacto`)
+VALUES (NULL, 'Juan', 'Lopez', '666666666', '21005', '25', 'V', 'Huelva', NULL);
+
+INSERT INTO `clientes` (`id`, `nombre`, `apellidos`, `telefono`, `codigo_postal`, `edad`, `sexo`, `ciudad`, `contacto`) VALUES (NULL, 'Ana', 'Ruiz', '66665454', '21001', '30', 'H', 'Huelva', "ella");
--- tema6/ejem1.php
+++ tema6/ejem1.php
@@ -1,11 +1,33 @@
 <?php
 try {
-   $conex = new PDO('mysql:host=127.0.0.1;dbname=bdusuarios', 'admin', 'admin');
-   $conex->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+   $db = new PDO('mysql:host=127.0.0.1;dbname=tema6', 'root', '');
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
    echo "La conexión se realizó correctamente!! :)";
 } catch (Exception $e) {
    echo 'Error : ' .  $e->GetMessage();
 } finally {
-   echo "Hola";
-   $base = null;
-}  //cierre de la conexión    
+   echo "<br>";
+}      
+
+$edad=29;
+// Datos FETCH_ASSOC 
+$stmt = $db->prepare("SELECT * FROM clientes WHER edad > :edad");
+// Especificamos el fetch mode antes de llamar a fetch() 
+// $stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->bindParam(":edad", $edad);
+// Ejecutamos la sentencia
+$stmt->execute();
+// Mostramos los resultados 
+while ($row = $stmt->fetch()) {
+   echo "Nombre: {$row[1]} <br>";
+   echo "Ciudad: {$row[7]} <br>";
+}
+
+$edad=15;
+$stmt->execute();
+// Mostramos los resultados 
+while ($row = $stmt->fetch()) {
+   echo "Nombre: {$row["nombre"]} <br>";
+   echo "Ciudad: {$row["ciudad"]} <br>";
+}
+
