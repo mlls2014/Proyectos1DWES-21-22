1fe4b78e576fc9633b90d8ca1fa65cdf5f2e7749
mlls2014 <manlucsan@gmail.com>
2021-11-16 13:14:38 +0100

    composer2

--- EjemComposer/index.php
+++ EjemComposer/index.php
@@ -1,11 +1,43 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelLow;
 use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Label\Label;
+use Endroid\QrCode\Logo\Logo;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
 
-$qrCode = new QrCode('Me encanta codigonaranja.com');
+$writer = new PngWriter();
 
-header('Content-Type: '.$qrCode->getContentType());
-echo $qrCode->writeString();
+// Create QR code
+$qrCode = QrCode::create('Data')
+    ->setEncoding(new Encoding('UTF-8'))
+    ->setErrorCorrectionLevel(new ErrorCorrectionLevelLow())
+    ->setSize(300)
+    ->setMargin(10)
+    ->setRoundBlockSizeMode(new RoundBlockSizeModeMargin())
+    ->setForegroundColor(new Color(0, 0, 0))
+    ->setBackgroundColor(new Color(255, 255, 255));
 
-?>
+// Create generic logo
+$logo = Logo::create(__DIR__.'/prueba.png')
+    ->setResizeToWidth(50);
+
+// Create generic label
+$label = Label::create('Label')
+    ->setTextColor(new Color(255, 0, 0));
+
+$result = $writer->write($qrCode, $logo, $label);
+
+// Directly output the QR code
+header('Content-Type: '.$result->getMimeType());
+echo $result->getString();
+
+// Save it to a file
+$result->saveToFile(__DIR__.'/qrcode.png');
+
+// Generate a data URI to include image data inline (i.e. inside an <img> tag)
+// $dataUri = $result->getDataUri();
